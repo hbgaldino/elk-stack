08d62ba28d69b06e1486e6ef75cd67515dd10334
Higor Galdino <higor.galdino@accurate.com.br>
2020-05-15 09:35:30 -0300

    Initial commit

---
+++ src/test/java/br/com/higor/elkstack/ElkStackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.higor.elkstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
